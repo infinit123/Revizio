57ddf8e31975176852ea553e2b3398cfe21c5c7c
infinit123 <161229244+infinit123@users.noreply.github.com>
2026-08-05 21:38:53 +0300

    Delete js directory

--- js/config.js
+++
@@ -1,9 +0,0 @@
-export const CONFIG = Object.freeze({
-  APP_NAME: 'Finora',
-  VERSION: '1.0.0',
-  DB_NAME: 'finora_db',
-  DB_VERSION: 1,
-  DEFAULT_MONTHLY_BUDGET: 2000,
-  DEFAULT_CURRENCY: '€',
-  AUTO_LOCK_IDLE_TIMEOUT_MS: 300000
-});
